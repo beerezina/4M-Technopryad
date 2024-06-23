df209197c7f9f12ff005a0515dc7f6c2cfe927f2
beerezina <berezinapk@yandex.ru>
2024-06-23 22:13:58 +0300

    111

---
+++ scripts/script-process.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuText = document.querySelector(".b-menu-text");
+  const blueMenu = document.querySelector(".blue-menu");
+  const closeMenuBtn = document.querySelector(".close-menu");
+  const patternScreen = document.querySelector(".b-pattern-screen");
+
+  menuText.addEventListener("click", function () {
+    if (blueMenu.style.display === "none" || blueMenu.style.display === "") {
+      blueMenu.style.display = "block";
+    } else {
+      blueMenu.style.display = "none";
+    }
+  });
+
+  closeMenuBtn.addEventListener("click", function () {
+    blueMenu.style.display = "none";
+  });
+
+  patternScreen.addEventListener("click", function (event) {
+    if (!blueMenu.contains(event.target) && !menuText.contains(event.target)) {
+      blueMenu.style.display = "none";
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const goToMainPageButton = document.querySelector(".b-go-to-main-page");
+
+  goToMainPageButton.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var box = document.getElementById("all-content-box-process");
+  box.addEventListener("click", function () {
+    window.location.href = "https://beerezina.github.io/Technopryad/";
+  });
+});
